fdab6e769153cac142e4b6a06ffaff3053ce55c2
Tamim2098 <145595412+Tamim2098@users.noreply.github.com>
2025-10-09 22:01:02 +0600

    About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -25,11 +25,7 @@ const About = () => {
             </p>
           </div>
 
-          <div className="about-skills">   
-            <div className="about-skill">
-              <p>C</p>
-              <hr style={{ width: '10%' }} />
-            </div>                
+          <div className="about-skills">                
             <div className="about-skill">
               <p>HTML & CSS</p>
               <hr style={{ width: '50%' }} />
